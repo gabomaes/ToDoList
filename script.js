8bbe43b5339b70019bd4919571b48a4ef9ce5c1f
gabomaes <98499641+gabomaes@users.noreply.github.com>
2022-06-30 23:15:54 -0400

    Add files via upload
    ToDoList final

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const CLAVE_LOCALSTORAGE = "lista_tareas";
 document.addEventListener("DOMContentLoaded", () => {
-	let tareas = []; 
-	
-	const $contenedorTareas = document.querySelector("#contenedorTareas"),
-		$btnGuardarTarea = document.querySelector("#btnAgregarTarea"),
-		$inputNuevaTarea = document.querySelector("#inputNuevaTarea");
+	let tareas = [];
 
-	
+
+	const $contenedorTareas = document.querySelector("#contenedorTareas");
+	$btnGuardarTarea = document.querySelector("#btnAgregarTarea");
+	$inputNuevaTarea = document.querySelector("#inputNuevaTarea");
+
+
+	$inputNuevaTarea.onkeyup = () => {
+		if ($inputNuevaTarea.value == ""){
+			$btnGuardarTarea.disabled = true;
+		} else{
+			$btnGuardarTarea.disabled = false;
+		}
+
+	}
+
 	$btnGuardarTarea.onclick = () => {
 		const tarea = $inputNuevaTarea.value;
 		if (!tarea) {
@@ -16,10 +26,12 @@ document.addEventListener("DOMContentLoaded", () => {
 			tarea: tarea,
 			terminada: false,
 		});
-		$inputNuevaTarea.value = "";
+	    $inputNuevaTarea.value = ("");
+		$btnGuardarTarea.disabled = true;
 		guardarTareasEnAlmacenamiento();
 		refrescarListaDeTareas();
 	};
+
 
 	const obtenerTareasDeAlmacenamiento = () => {
 		const posibleLista = JSON.parse(localStorage.getItem(CLAVE_LOCALSTORAGE));
@@ -48,14 +60,14 @@ document.addEventListener("DOMContentLoaded", () => {
 					return;
 				}
 				tareas.splice(indice, 1);
-				
+
 				guardarTareasEnAlmacenamiento(tareas);
 				refrescarListaDeTareas();
 			};
-			
+
 			const $checkbox = document.createElement("input");
 			$checkbox.type = "checkbox";
-			$checkbox.onchange = function () { 
+			$checkbox.onchange = function () {
 				if (this.checked) {
 					tareas[indice].terminada = true;
 				} else {
@@ -65,15 +77,16 @@ document.addEventListener("DOMContentLoaded", () => {
 				refrescarListaDeTareas();
 			}
 
-			
+
 			const $span = document.createElement("span");
 			$span.textContent = tarea.tarea;
-			
+
 			const $li = document.createElement("li");
-			
+
 			if (tarea.terminada) {
 				$checkbox.checked = true;
 				$span.classList.add("tachado");
+				$enlaceParaEliminar.innerHTML = "";
 			}
 			$li.appendChild($checkbox);
 			$li.appendChild($span);
@@ -81,7 +94,12 @@ document.addEventListener("DOMContentLoaded", () => {
 			$contenedorTareas.appendChild($li);
 		}
 	};
-	
+
 	tareas = obtenerTareasDeAlmacenamiento();
 	refrescarListaDeTareas();
+
+
+	if ($inputNuevaTarea.value == ""){
+		$btnGuardarTarea.disabled = true;
+	} 
 });
